5a8613071ee235e47bcadf2fb93896dbe270dfcc
adamhinton <adam.d.hinton@gmail.com>
2025-10-26 12:17:45 -0400

    Readme

--- app/recommend/actions.ts
+++ app/recommend/actions.ts
@@ -1,7 +1,15 @@
+// ________________________________
+// This is a testing file for now
+// User will enter a lichess username on the client;
+// This server action will call the Liches (or other site) API to get game data for that username
+// Then, it will process that data.
+// Right now, we just want to successfully download game data.
+// ________________________________
+
 "use server";
 
 export async function processLichessUsername(formData: FormData) {
-	const username = formData.get("username") as string;
+	const username = formData.get("username");
 
 	if (!username) {
 		throw new Error("Username is required");
