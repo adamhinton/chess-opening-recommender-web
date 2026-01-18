71a7f9503a2d85c6ec003da9368b97a87180257f
adamhinton <adam.d.hinton@gmail.com>
2026-01-18 09:59:09 -0500

    remove deprecated app route

--- app/display_recommendations/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-// ===============================
-// Displays chess opening recommendations generated based on user stats
-// ===============================
-
-const DisplayRecommendationsPage = () => {};
-
-export default DisplayRecommendationsPage;
